3c08de571ea646d070ee457c2ae7d2f3d942f669
IfeMartin <114827202+IfeMartin@users.noreply.github.com>
2025-09-08 11:57:08 -0400

    Add Amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dev": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
